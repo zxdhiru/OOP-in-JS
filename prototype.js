fd723da97a8ae4a7d5856312dbfaa962114f4710
Dhiru <dhiru@alljobguider.in>
2024-10-08 09:51:36 +0530

    link script to html

--- prototype.js
+++ prototype.js
@@ -10,3 +10,9 @@ BankAccount.prototype.deposit = function (amount) {
 BankAccount.prototype.withdraw = function (amount) {
     this.balance -= amount
 }
+
+const dhiruAccount = new BankAccount("Dhiru", 500)
+dhiruAccount.deposit(6000)
+dhiruAccount.withdraw(18)
+
+console.log(dhiruAccount);
