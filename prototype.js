1b46d3652315098a0749fd493683312cc928fa96
Dhiru <dhiru@alljobguider.in>
2024-10-08 09:49:05 +0530

    add deposit and withdraw prototype

--- prototype.js
+++ prototype.js
@@ -2,6 +2,11 @@ function BankAccount(customerName, balance = 0) {
     this.customerName = customerName,
         this.accountNumber = Math.floor(Date.now() + Math.random()),
         this.balance = balance
+}
 
-
+BankAccount.prototype.deposit = function (amount) {
+    this.balance += amount
+}
+BankAccount.prototype.withdraw = function (amount) {
+    this.balance -= amount
 }
